52dea6f2b263061ac638026dcac2f7e7098d3f9b
theblakeyg <theblakeyg@gmail.com>
2020-09-05 00:07:26 -0500

    Added ItemController with full functionality

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Item> model = _context.Items.ToList();
+
+            return View("Index", model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var itemToDelete = _context.Items.FirstOrDefault(o => o.Id == id);
+
+            _context.Items.Remove(itemToDelete);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
